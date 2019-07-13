aec7a2f9860430a813c9ab81f8cad207e5b784e8
Witthayathorn Sompor <tamimph2541@gmail.com>
2019-07-13 14:13:52 +0700

    เพิ่มหน้าเทส

--- index.js
+++ index.js
@@ -5,5 +5,9 @@ const port = 3000
 app.get('/', (req, res) => res.send('พะเยาฮาเก๋าคนเหล่ว'))
 app.get('/about', (req, res) => res.send('สันกำแปงฮาเก๋าคนเหล่ว'))
 app.get('/Contact', (req, res) => res.send('0909459216'))
+app.get('/test', (req, res) => {
+    res.json({message: "test"})
+})
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
